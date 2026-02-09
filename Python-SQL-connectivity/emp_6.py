490720ec1c7e9d7550a6dddf4d30979c1ea2b99c
sakshipetkar2812-jpg <sakshipetkar2812@gmail.com>
2026-02-09 19:55:07 +0530

    Add files via upload

---
+++ Python-SQL-connectivity/emp_6.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import pandas as pd
+import numpy as np
+
+conn= mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="7984455774sak",
+    database="employee"
+)
+cursor=conn.cursor()
+print("connected to database")
+
+# step_19: Create a view for employee with salaries greater than 6000 in different countries
+cursor.execute("""
+SELECT ROLE,MIN(SALARY) AS MIN_SALARY, MAX(SALARY) AS MAX_SALARY FROM emp_record_table group by ROLE;
+""") 
+rows= cursor.fetchall()
+
+print("\n Minimum and Maximum salary")
+print("Role | min | max")
+print("-"*80)
+for row in rows:
+    print("|".join(map(str,row)))
+
+#step_20 : Close MYsql connection
+cursor.close()
+conn.close()
+print("All tasks completed")
+
